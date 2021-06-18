517bb067317e8d370b277068c59afe71aec7d3e7
Tagaiatto <34107323+Tagaiatto@users.noreply.github.com>
2021-06-18 14:40:55 -0300

    Adicionado link na logo do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link';
 import styles from './header.module.scss';
 
 export default function Header(): JSX.Element {
   return (
     <header className={styles.container}>
-      <img src="/images/logo.png" alt="logo" />
+      <Link href="/">
+        <button type="button">
+          <img src="/images/logo.png" alt="logo" />
+        </button>
+      </Link>
     </header>
   );
 }
